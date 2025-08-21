813bc80a3b9db5b91f4ae95b78fc5af2a1ae343a
johamja <johamfranco318@gmail.com>
2025-08-21 14:17:04 -0500

    Tarea 1: Primer ejemplo con el GPS

---
+++ src/main.cpp
@@ -0,0 +1,65 @@
+#include <Arduino.h>
+
+#include <TinyGPSPlus.h>
+#include <SoftwareSerial.h>
+/*
+   This sample code demonstrates the normal use of a TinyGPSPlus (TinyGPSPlus) object.
+   It requires the use of SoftwareSerial, and assumes that you have a
+   4800-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
+*/
+static const int RXPin = 2, TXPin = 0;
+static const uint32_t GPSBaud = 9600;
+
+// The TinyGPSPlus object
+TinyGPSPlus gps;
+
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
+
+
+
+void setup() {
+
+  Serial.begin(115200);
+  ss.begin(GPSBaud);
+
+  Serial.println(F("FullExample.ino"));
+  Serial.println(F("An extensive example of many interesting TinyGPSPlus features"));
+  Serial.print(F("Testing TinyGPSPlus library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
+  Serial.println(F("by Mikal Hart"));
+  Serial.println();
+  Serial.println(F("Sats HDOP  Latitude   Longitude   Fix  Date       Time     Date Alt    Course Speed Card  Distance Course Card  Chars Sentences Checksum"));
+  Serial.println(F("           (deg)      (deg)       Age                      Age  (m)    --- from GPS ----  ---- to London  ----  RX    RX        Fail"));
+  Serial.println(F("----------------------------------------------------------------------------------------------------------------------------------------"));
+}
+
+void smartDelay(unsigned long ms);
+
+void loop() {
+   smartDelay(100);
+  
+  Serial.print(gps.location.lng());
+  Serial.print(" ");
+  Serial.print(gps.location.lat());
+  Serial.print(" ");
+  Serial.print(gps.altitude.meters());
+    Serial.print(" ");
+  
+  char sz[32];
+  sprintf(sz, "%02d/%02d/%02d ", gps.date.month(), gps.date.day(), gps.date.year());
+  Serial.print(sz);
+  Serial.print(" ");
+
+}
+
+// This custom version of delay() ensures that the gps object
+// is being "fed".
+void smartDelay(unsigned long ms)
+{
+  unsigned long start = millis();
+  do 
+  {
+    while (ss.available())
+      gps.encode(ss.read());
+  } while (millis() - start < ms);
+}
