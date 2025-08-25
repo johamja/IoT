49aa63f02ebdbde7bcb91ea6ea6e0d618a180afe
johamja <johamfranco318@gmail.com>
2025-08-25 10:12:51 -0500

    Tarea 2: Ejemplo con el sensor de temperatura

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
 #include <Arduino.h>
-
+#include <Wire.h>
 #include <TinyGPSPlus.h>
 #include <SoftwareSerial.h>
+#include "ClosedCube_HDC1080.h"
+
+ClosedCube_HDC1080 hdc1080;
 /*
    This sample code demonstrates the normal use of a TinyGPSPlus (TinyGPSPlus) object.
    It requires the use of SoftwareSerial, and assumes that you have a
    4800-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
 */
-static const int RXPin = 2, TXPin = 0;
+static const int RXPin = 4, TXPin = 3;
 static const uint32_t GPSBaud = 9600;
 
 // The TinyGPSPlus object
@@ -16,7 +19,9 @@ TinyGPSPlus gps;
 // The serial connection to the GPS device
 SoftwareSerial ss(RXPin, TXPin);
 
+void smartDelay(unsigned long ms);
 
+void printSerialNumber();
 
 void setup() {
 
@@ -31,12 +36,26 @@ void setup() {
   Serial.println(F("Sats HDOP  Latitude   Longitude   Fix  Date       Time     Date Alt    Course Speed Card  Distance Course Card  Chars Sentences Checksum"));
   Serial.println(F("           (deg)      (deg)       Age                      Age  (m)    --- from GPS ----  ---- to London  ----  RX    RX        Fail"));
   Serial.println(F("----------------------------------------------------------------------------------------------------------------------------------------"));
+
+	Serial.println("ClosedCube HDC1080 Arduino Test");
+
+	// Default settings: 
+	//  - Heater off
+	//  - 14 bit Temperature and Humidity Measurement Resolutions
+	hdc1080.begin(0x40);
+
+	Serial.print("Manufacturer ID=0x");
+	Serial.println(hdc1080.readManufacturerId(), HEX); // 0x5449 ID of Texas Instruments
+	Serial.print("Device ID=0x");
+	Serial.println(hdc1080.readDeviceId(), HEX); // 0x1050 ID of the device
+	
+	printSerialNumber();
+
 }
 
-void smartDelay(unsigned long ms);
 
 void loop() {
-   smartDelay(100);
+  smartDelay(100);
   
   Serial.print(gps.location.lng());
   Serial.print(" ");
@@ -50,6 +69,12 @@ void loop() {
   Serial.print(sz);
   Serial.print(" ");
 
+  Serial.print("T=");
+	Serial.print(hdc1080.readTemperature());
+	Serial.print("C, RH=");
+	Serial.print(hdc1080.readHumidity());
+	Serial.println("%");
+
 }
 
 // This custom version of delay() ensures that the gps object
@@ -62,4 +87,12 @@ void smartDelay(unsigned long ms)
     while (ss.available())
       gps.encode(ss.read());
   } while (millis() - start < ms);
+}
+
+void printSerialNumber() {
+	Serial.print("Device Serial Number=");
+	HDC1080_SerialNumber sernum = hdc1080.readSerialNumber();
+	char format[12];
+	sprintf(format, "%02X-%04X-%04X", sernum.serialFirst, sernum.serialMid, sernum.serialLast);
+	Serial.println(format);
 }
