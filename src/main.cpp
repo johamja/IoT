65d975649a420be9ff256cb88f965102f7409e61
johamja <johamfranco318@gmail.com>
2025-08-29 15:21:11 -0500

    https funciona

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,11 @@
 #include <SoftwareSerial.h>
 #include "ClosedCube_HDC1080.h"
 #include <ESP8266WiFi.h>
+#include <WiFiClientSecureBearSSL.h>
 #include <map>
 #include <mbedtls/sha256.h> // Para el hash SHA-256
-#include <base64.h>			// Para codificar el cifrado (simulación)
-#include <time.h>			// Para timestamp
+#include <base64.h>         // Para codificar el cifrado (simulación)
+#include <time.h>           // Para timestamp
 #include <ArduinoJson.h>
 #include "mbedtls/aes.h"
 #include <Hash.h>
@@ -15,7 +16,38 @@
 // Constantes para usar WiFi
 const char *ssid = "joham";
 const char *password = "joham1015";
-const char *url = "98.85.128.81";
+const char *url = "10.199.26.8";
+const int httpsPort = 443;
+// SHA1 Fingerprint del certificado
+const char fingerprint[] PROGMEM = "F4 FD EB 03 71 10 48 F0 D7 CA A0 1A FD 14 8B 65 D8 91 3D FC";
+// Pegar aquí tu certificado PEM
+static const char digicert_root_ca[] PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIID4zCCAsugAwIBAgIUBU7/ESsV9/fNwuEJbS+xBJvmokYwDQYJKoZIhvcNAQEL
+BQAwgYAxCzAJBgNVBAYTAkNPMRMwEQYDVQQIDApTb21lLVN0YXRlMREwDwYDVQQH
+DAhtZWRlbGxpbjEMMAoGA1UECgwDdXBiMQwwCgYDVQQLDAN1cGIxDDAKBgNVBAMM
+A2lvdDEfMB0GCSqGSIb3DQEJARYQbGFiaW90QGdtYWlsLmNvbTAeFw0yNTA4Mjkx
+ODA2MThaFw0yNjA4MjkxODA2MThaMIGAMQswCQYDVQQGEwJDTzETMBEGA1UECAwK
+U29tZS1TdGF0ZTERMA8GA1UEBwwIbWVkZWxsaW4xDDAKBgNVBAoMA3VwYjEMMAoG
+A1UECwwDdXBiMQwwCgYDVQQDDANpb3QxHzAdBgkqhkiG9w0BCQEWEGxhYmlvdEBn
+bWFpbC5jb20wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQC0ko+SqSCd
+Bgs9t3ID0GuNpbuehIz+sFqMK5q5PoZQpNylYRLSXiJEMNTYbHc0SR/MipLrXPZU
+CjuQ2IuyzyA+rO1TakDy3UYhq8sAkTE3Ey7Z9m0N235uxeShoPQBc6yeeJ/S/P1n
+LqPeSFVqkOvy3GyyOidxRkAcr+lPtcq4fM2FrW5/GibVzrPq+viXv8AyGi7QNUo3
+jM7/mSK9pctOS67gZUzkNiY3KGvPEopgp9TOVjNb/Yv56wY0hFizzKIxbDoztW7d
+TdxCIQbbCtgaZSHL2pzV4Cuau1mwys0BPMtA5Ve3u3GbYA1vZIRgmjLQPtWfnE96
+t/pTGn1CBu3LAgMBAAGjUzBRMB0GA1UdDgQWBBRvW+eQJtT7T31WLqBUvnF1NqR8
+SzAfBgNVHSMEGDAWgBRvW+eQJtT7T31WLqBUvnF1NqR8SzAPBgNVHRMBAf8EBTAD
+AQH/MA0GCSqGSIb3DQEBCwUAA4IBAQCWhbquKbUkV+rCwsv3jTid7VxJqHhVSYTg
+Z/YdwUhih+j74eIsbVmZFcTkzVr2SHUcQ+4BvVL5yFojXu4CYFA65EMpRbVOB1+x
+dXsEmF1REAB4lPjqsblWuim93Pf/w/JBQrDe1GuKBWowRIZKUPUVkLdKDOlcBHcA
+/uvvh8y6MWhRw1g2t2+QgKTwNLC/PkWDmoWzI8gYBt5c8ve0euVd3EsgNxToXYCV
+OINRhDPVobBjzOTc5lOVyA1Mefcafk7Qzrgmjlmnj2NlNR2eOitkdc4z6L2LBNGj
+TNQUDOYrVJfIR1/oUU90W2RdxeRuAEgdjy8P6iTJO4bdEXuW8EBu
+-----END CERTIFICATE-----
+)EOF";
+
+WiFiClientSecure client;
 
 // Constates para usar el GPS
 static constexpr int RXPin = 2, TXPin = 0;
@@ -31,7 +63,7 @@ const char *id = "device-01";
 const char *type = "sensor-humedad-temperatura";
 
 String output;
-int sample = 10;				 // Número de muestras a tomar
+int sample = 10;                 // Número de muestras a tomar
 int time_between_samples = 1000; // Tiempo entre muestras en milisegundos
 double temperature_list[10];
 double humidity_list[10];
@@ -42,12 +74,12 @@ double longitud = 0;
 
 struct Datos
 {
-	double longitud;
-	double latitud;
+    double longitud;
+    double latitud;
 };
 
 auto cmp = [](const double a, const double b)
-{ return a < b; };									// Comparador para ordenar de menor a mayor
+{ return a < b; };                                  // Comparador para ordenar de menor a mayor
 std::map<double, Datos, decltype(cmp)> miMapa(cmp); // Mapa con llave int y valor Datos, ordenado de mayor a menor
 
 // Declaración de metodos
@@ -67,24 +99,27 @@ String generateSHA256(const String &data);
 void setup()
 {
 
-	// 4
-	hdc1080.begin(0x40);
+    // 4
+    hdc1080.begin(0x40);
 
-	// 1
-	Serial.begin(115200);
+    // 1
+    Serial.begin(115200);
 
-	// 2
-	Serial.println("Connecting to " + *ssid);
-	WiFi.begin(ssid, password);
-	while (WiFi.status() != WL_CONNECTED)
-	{
-		delay(500);
-		Serial.print(".");
-	}
-	Serial.println("WiFi connected");
+    // 2
+    Serial.println("Connecting to " + *ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("WiFi connected");
+    BearSSL::X509List cert(digicert_root_ca);
+    client.setTrustAnchors(&cert);
+    client.setFingerprint(fingerprint);
 
-	// 3
-	gpsInput.begin(GPSBaud);
+    // 3
+    gpsInput.begin(GPSBaud);
 }
 
 int state = 1;
@@ -115,143 +150,149 @@ int state = 1;
  */
 void loop()
 {
-	switch (state)
-	{
-	case 1:
-	{
-		Serial.println("Iniciando CHIRP de temperatura y humedad");
-		for (int i = 0; i < sample; i++)
-		{
-			temperature_list[i] = hdc1080.readTemperature();
-			humidity_list[i] = hdc1080.readHumidity();
-			Serial.printf("\tMuestra %d Temperatura %lf Humedad %lf \n", i, temperature_list[i], humidity_list[i]);
-			delay(time_between_samples);
-		}
-		state = 2;
-	}
+    switch (state)
+    {
+    case 1:
+    {
+        Serial.println("Iniciando CHIRP de temperatura y humedad");
+        for (int i = 0; i < sample; i++)
+        {
+            temperature_list[i] = hdc1080.readTemperature();
+            humidity_list[i] = hdc1080.readHumidity();
+            Serial.printf("\tMuestra %d Temperatura %lf Humedad %lf \n", i, temperature_list[i], humidity_list[i]);
+            delay(time_between_samples);
+        }
+        state = 2;
+    }
 
-	break;
+    break;
 
-	case 2:
-	{
-		Serial.println("Iniciando PRUNING de temperatura y humedad");
-		double temperature_sum = 0;
-		double humidity_sum = 0;
-		for (int i = 0; i < sample; i++)
-		{
-			temperature_sum += temperature_list[i];
-			humidity_sum += humidity_list[i];
-		}
-		temperature_avg = temperature_sum / sample;
-		humidity_avg = humidity_sum / sample;
-		Serial.println("Temperatura promedio: " + String(temperature_avg));
-		Serial.println("Humedad promedio: " + String(humidity_avg));
-		state = 3;
-	}
-	break;
+    case 2:
+    {
+        Serial.println("Iniciando PRUNING de temperatura y humedad");
+        double temperature_sum = 0;
+        double humidity_sum = 0;
+        for (int i = 0; i < sample; i++)
+        {
+            temperature_sum += temperature_list[i];
+            humidity_sum += humidity_list[i];
+        }
+        temperature_avg = temperature_sum / sample;
+        humidity_avg = humidity_sum / sample;
+        Serial.println("Temperatura promedio: " + String(temperature_avg));
+        Serial.println("Humedad promedio: " + String(humidity_avg));
+        state = 3;
+    }
+    break;
 
-	case 3:
-	{
-		Serial.println("Iniciando CHIRP del GPS");
-		for (int i = 0; i < sample; i++)
-		{
-			smartDelay(time_between_samples);
+    case 3:
+    {
+        Serial.println("Iniciando CHIRP del GPS");
+        for (int i = 0; i < sample; i++)
+        {
+            smartDelay(time_between_samples);
 
-			latitud = gps.location.lat();
-			longitud = gps.location.lng();
-			const double error = gps.hdop.hdop();
-			const double estimatedError = error * 5.0; // 5 meters is a common base error
+            latitud = gps.location.lat();
+            longitud = gps.location.lng();
+            const double error = gps.hdop.hdop();
+            const double estimatedError = error * 5.0; // 5 meters is a common base error
 
-			Serial.printf("\t Muestra %d Lat %lf Lon %lf Error %lf\n", i, latitud, longitud, estimatedError);
+            Serial.printf("\t Muestra %d Lat %lf Lon %lf Error %lf\n", i, latitud, longitud, estimatedError);
 
-			miMapa[estimatedError] = {longitud, latitud};
-		}
-		state = 4;
-	}
+            miMapa[estimatedError] = {longitud, latitud};
+        }
+        state = 4;
+    }
 
-	break;
+    break;
 
-	case 4:
-	{
-		Serial.println("Iniciando PRUNING del GPS");
-		const double error = miMapa.begin()->first;
-		latitud = miMapa.begin()->second.latitud;
-		longitud = miMapa.begin()->second.longitud;
+    case 4:
+    {
+        Serial.println("Iniciando PRUNING del GPS");
+        const double error = miMapa.begin()->first;
+        latitud = miMapa.begin()->second.latitud;
+        longitud = miMapa.begin()->second.longitud;
 
-		Serial.println("Latitud: " + String(latitud));
-		Serial.println("Longitud: " + String(longitud));
-		Serial.println("Error estimado: " + String(error) + " metros");
-		state = 5;
-	}
+        Serial.println("Latitud: " + String(latitud));
+        Serial.println("Longitud: " + String(longitud));
+        Serial.println("Error estimado: " + String(error) + " metros");
+        state = 5;
+    }
 
-	break;
+    break;
 
-	case 5:
-	{
+    case 5:
+    {
 
-		Serial.println("Iniciando BUNDLING");
+        Serial.println("Iniciando BUNDLING");
 
-		JsonDocument data;
-		data["id"] = "point11";
-		data["lat"] = latitud;
-		data["lon"] = longitud;
-		data["temperatura"] = temperature_avg;
-		data["humedad"] = humidity_avg;
-		serializeJson(data, output);
+        JsonDocument data;
+        data["id"] = "point14";
+        data["lat"] = latitud;
+        data["lon"] = longitud;
+        data["temperatura"] = temperature_avg;
+        data["humedad"] = humidity_avg;
+        serializeJson(data, output);
 
-		Serial.println("Paquete final:");
-		Serial.println(output);
-		state = 6;
-	}
-	break;
+        Serial.println("Paquete final:");
+        Serial.println(output);
+        state = 6;
+    }
+    break;
 
-	case 6:
-	{
-		Serial.println("Enviando la trama");
-		WiFiClient client;
+    case 6:
+    {
+        Serial.println("Enviando la trama");
 
-		if (client.connect(url, 80))
-		{
-			Serial.println("conectado");
-			client.println("POST /update_data HTTP/1.1");
-			client.println("Host: 10.15.69.51");
-			client.println("User-Agent: Arduino/1.0");
-			client.println("Connection: close");
-			client.println("Content-Type: application/json");
-			client.print("Content-Length: ");
-			client.println(output.length());
-			client.println();		// Línea vacía entre encabezados y cuerpo
-			client.println(output); // Envía el cuerpo
-		}
-		else
-		{
-			Serial.println("error de conexion");
-		}
-		state = 7;
-	}
-	break;
+        if (!client.connect(url, httpsPort))
+        {
+            Serial.println("Conexión fallida a servidor HTTPS");
+            return;
+        }
 
-	case 7:
-	{
-		Serial.println("Durmiendo 10 segundos");
-		delay(10000);
-		state = 1;
-	}
-	break;
+        Serial.println("conectado");
+        client.println("POST /update_data HTTP/1.1");
+        client.println("Host: 10.15.69.51");
+        client.println("User-Agent: Arduino/1.0");
+        client.println("Connection: close");
+        client.println("Content-Type: application/json");
+        client.print("Content-Length: ");
+        client.println(output.length());
+        client.println();       // Línea vacía entre encabezados y cuerpo
+        client.println(output); // Envía el cuerpo
 
-	
-	default:
-		Serial.println("Opcion por defecto");
-	}
+        // Leer respuesta del servidor
+        while (client.connected() || client.available())
+        {
+            String line = client.readStringUntil('\n');
+            Serial.println(line);
+            if (line == "{\"success\": true}") break;
+        }
+
+        state = 7;
+    }
+    break;
+
+    case 7:
+    {
+        Serial.println("Durmiendo 10 segundos");
+        delay(10000);
+        state = 1;
+    }
+    break;
+
+    default:
+        Serial.println("Opcion por defecto");
+    }
 }
 
 // Metodos
 void smartDelay(const unsigned long ms)
 {
-	const unsigned long start = millis();
-	do
-	{
-		while (gpsInput.available())
-			gps.encode(gpsInput.read());
-	} while (millis() - start < ms);
+    const unsigned long start = millis();
+    do
+    {
+        while (gpsInput.available())
+            gps.encode(gpsInput.read());
+    } while (millis() - start < ms);
 }
